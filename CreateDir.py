e1973cc43eec3094e26e1906dbbce94ddab0d817
ZumbiPy <zumbipy@gmail.com>
2018-01-29 05:12:13 -0200

    Update
    Nova logica na fun;áo criar_nomes()

--- CreateDir.py
+++ CreateDir.py
@@ -4,45 +4,48 @@ import os
 class CreateDir(object):
 
     def __init__(self, numero_pasta, *nome_pasta, prefixo=False, sufixo=False, inicio=1):
+        self.inicio = inicio
+        self.numero_pasta = numero_pasta + 1
         self.prefixo = prefixo
         self.sufixo = sufixo
-        self.inicio = inicio
-        self.numero_pasta = numero_pasta
         self.nome_pasta = nome_pasta
         self.lista_nome_pastas = []
 
     def criar_nomes(self):
-        if not self.lista_nome_pastas:
+        if self.prefixo:
             nome = self.nome_pasta
-            if self.prefixo and self.sufixo:
-                for pastas in range(self.inicio, self.numero_pasta + 1):
-                    if pastas <= len(nome):
-                        self.lista_nome_pastas.append(f'{pastas}{nome[pastas-1]}{pastas}')
-                    else:
-                        self.lista_nome_pastas.append(f'{pastas}')
-
-            elif self.prefixo:
-                for pastas in range(self.inicio, self.numero_pasta + 1):
-                    if pastas <= len(nome):
-                       self.lista_nome_pastas.append(f'{pastas}{nome[pastas-1]}')
-                    else:
-                        self.lista_nome_pastas.append(f'{pastas}')
-
-            elif self.sufixo:
-                for pastas in range(self.inicio, self.numero_pasta + 1):
-                    if pastas <= len(nome):
-                       self.lista_nome_pastas.append(f'{nome[pastas-1]}{pastas}')
-                    else:
-                        self.lista_nome_pastas.append(f'{pastas}')
-            elif len(self.nome_pasta) is 1:
-                for pastas in range(self.inicio, self.numero_pasta + 1):
-                    self.lista_nome_pastas.append(f'{self.nome_pasta[0]}')
+            if len(self.nome_pasta) == 1:
+                for n in range(self.inicio, self.numero_pasta):
+                    self.lista_nome_pastas.append(f"{n}{nome[0]}")
             else:
-                for pastas in range(self.inicio, self.numero_pasta + 1):
-                    self.lista_nome_pastas.append(f'{pastas}')
+                for n in range(self.inicio, self.numero_pasta):
+                    if n <= len(self.nome_pasta):
+                        self.lista_nome_pastas.append(f"{n}{nome[n - 1]}")
+                    else:
+                        self.lista_nome_pastas.append(f"{n}")
+        elif self.sufixo:
+            nome = self.nome_pasta
+            if len(self.nome_pasta) == 1:
+                for n in range(self.inicio, self.numero_pasta):
+                    self.lista_nome_pastas.append(f"{nome[0]}{n}")
+            else:
+                for n in range(self.inicio, self.numero_pasta):
+                    if n <= len(self.nome_pasta):
+                        self.lista_nome_pastas.append(f"{nome[n - 1]}{n}")
+                    else:
+                        self.lista_nome_pastas.append(f"{n}")
 
-    def limpar(self):
-        self.lista_nome_pastas = []
+        elif (self.numero_pasta - 1) == 0:
+            for n in self.nome_pasta:
+                self.lista_nome_pastas.append(f"{n}")
+
+        elif self.nome_pasta:
+            nome = self.nome_pasta
+            for n in range(self.inicio, self.numero_pasta):
+                self.lista_nome_pastas.append(f"{nome[0]}")
+        else:
+            for n in range(self.inicio, self.numero_pasta):
+                self.lista_nome_pastas.append(f"{n}")
 
     def criar_pastas(self):
         for pastas in self.lista_nome_pastas:
@@ -51,11 +54,3 @@ class CreateDir(object):
     def visualizar(self):
         for nomes in self.lista_nome_pastas:
             print(nomes)
-"""
-    def verificando_lista(self, nome_pasta):
-        if len(nome_pasta) == 1:
-            nome = nome_pasta[0]
-            nome_pasta = [nome for nome in range(self.numero_pasta)]
-        else:
-            return nome_pasta
-"""
