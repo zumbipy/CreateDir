e0915580fc17780806e54e2fb8317b012c6ef183
ZumbiPy <zumbipy@gmail.com>
2018-02-02 04:13:22 -0200

    Versao final 1.0

--- CreateDir.py
+++ CreateDir.py
@@ -1,46 +1,41 @@
 import os
+import argparse
 
 
 class CreateDir(object):
-    """ Esta Classe criar um nome com padrões definidos pelo o usuario.
-    Tendo como objetivo agilizar a criação de varias pastas com nomes parecidos ou ate diferente 
-    com numeração no começo ou no final.
+    """Esta Classe criar um nome ou nomes definidos pelo usuário.
+        Tendo como objectivo agilizar a criação de varias pastas com nomes parecidos ou ate diferentes
+        com numeração no começo ou no final
+    """
 
-    Paramentro Obrigatorio:
-        total_pasta: Valor *Obrigatorio ,Servi para sabemos quantas pastas seram criadas
-                    se valor for 0 as pastas não deram numeros.
-
-    Parametros Opcionais:
-        nome_pasta: Nome da pasta ou Listas de nomes de pasta
-        prefixo: Por padãro esta em True, Para Desativa False para desativa
-        sufico: Por padrão False, Para ativa colore True
-        inicio: Por padrão 0 quaso a pasta começe com outra numerção e so coloca o número
-        """
-
-    def __init__(self, total_pasta, nome_pasta, prefixo=True, sufixo=False, inicio=1):
+    def __init__(self, total_pasta, nome_pasta, prefixo=False, inicio=1, sim=False):
         # Variaveis resposavel para criação do nome das pastas.
         self.prefixo = prefixo
-        self.sufixo = sufixo
-        self.lista_nome_pastas = list(nome_pasta)
+        self.sufixo = True if not prefixo else False  # prefixo verdade sufixo falso
+        self.lista_nome_pastas = nome_pasta
 
         # Váriaveis para criação das do total de pasta.
         self.total_pasta = total_pasta
         self.inicio = inicio
 
+        # confirmação de craição de pasta
+        self.sim_ou_nao = sim
+
         # Métodos.
-        self.correcao_nomes_pastas()
+        self.nomeando_pastas()
         self.criar_nomes()
 
     def criar_nomes(self):
-        """ Criar o nome das pastas"""
+        """ Craindo nomes nas pastas."""
         self.prefixo_nome_sufixo()
         self.visualizar()
-        self.criar_pastas()
 
+        # Criar pasta só se self.total_pasta for maior que 0.
+        if self.lista_nome_pastas:
+            self.criar_pastas()
 
     def criar_pastas(self):
         """ Criar as pastas no OS."""
-        
         if self.sim_nao():
             for pasta in self.lista_nome_pastas:
                 os.mkdir(pasta)
@@ -50,46 +45,28 @@ class CreateDir(object):
         for nome in self.lista_nome_pastas:
             print(nome)
 
-    def correcao_nomes_pastas(self):
-        """ Pega a lista de nome de pasta e deixa do mesmo tamanho do total de pastas a se criado"""
-
-        # Aqui crio uma variavel que recebe a diferença de valor entre total_pasta com lista_nome_pastas.
-        t_pasta = self.total_pasta - len(self.lista_nome_pastas)
-
-        # Se lista_nome_pastas tive so um elemento o mesmo sera nome de todas as pastas.
-        if len(self.lista_nome_pastas) == 1:
+    def nomeando_pastas(self):
+        len_nome_pasta = len(self.lista_nome_pastas)
+        if len_nome_pasta == 1:
             nome = self.lista_nome_pastas[0]
-            for i in range(t_pasta):
-                self.lista_nome_pastas.append(nome)
-
-        # Se lista_nome_pastas for 0 então as pasta "" <-- str() vazia.
-        elif len(self.lista_nome_pastas) == 0:
-            nome = ''
-            for i in range(t_pasta):
-                self.lista_nome_pastas.append(nome)
-
-        # Existe a possibilidade da pasta não te nome se uma str '' o OS não aceita dando erro então
-        # Para resolver este problema o Codigo Repete o nome "Sem Nome" no lugar da str
-        elif not(self.prefixo) and self.total_pasta > len(self.lista_nome_pastas) and not(self.sufixo):
-            nome = "Sem Nome"
-            for i in range(t_pasta):
-                self.lista_nome_pastas.append(nome)
-        # Ultimo caso e quando pasta não tem nome é ele sera o numero.
+            self.lista_nome_pastas = [nome] * self.total_pasta
         else:
-            for i in range(t_pasta):
+            for numero in range(self.total_pasta - len_nome_pasta):
                 self.lista_nome_pastas.append("")
 
     def prefixo_nome_sufixo(self):
-        """ Adicionado prefixo ou sufixo no nome atual lista_nome_pastas. """
+        """ Pega um elemento dentro da lista_nome_pasta e acrecenta prefixo ou sufixo. """
         contador = 0
-        for numero in range(self.inicio, self.total_pasta + 1):
 
+        for numero in range(self.inicio, (self.total_pasta + self.inicio)):
+
+            # Validação se True retorna numero se False retorna nada.
             prefixo = self.verdade_ou_falso(self.prefixo, numero)
             sufixo = self.verdade_ou_falso(self.sufixo, numero)
 
+            # Pega o valor e o modificar.
             nome_pasta = self.lista_nome_pastas[contador]
-            novo_nome = f"{prefixo}{nome_pasta}{sufixo}"
-            self.lista_nome_pastas[contador] = novo_nome
+            self.lista_nome_pastas[contador] = f"{prefixo}{nome_pasta}{sufixo}"
 
             contador += 1
 
@@ -104,13 +81,56 @@ class CreateDir(object):
 
     def sim_nao(self):
         """ Verificar se tem certeza que vai criar as pastas no OS """
+        if self.sim_ou_nao:
+            return True
         while True:
             sim_nao = input("Criar pastas [s] - sim ou [n] - não: ").lower()
             if sim_nao == 's':
                 return True
                 break
-            if sim_nao =='n':
+            if sim_nao == 'n':
                 return False
                 break
 
-        
+
+comados = argparse.ArgumentParser(prog="cdir.py",
+                                  usage='''cdir.py *[Numero De Pastas] -n 'Nome' -p -i [numero]
+
+Criando varias pastas facilmete por linha de comando.
+Obs: "Primeiro valor é Obrigadorio."""
+
+Comandos Básicos:
+    cdir.py 3 -n " - Aula"
+        Aula - 1
+        Aula - 2
+        Aula - 3
+
+    cdir.py 3 -n "Aula - " -p
+        1 - Aula
+        2 - Aula
+        3 - Aula
+
+     cdir.py 3 -n "Aula - " "Curso - " -i 100
+        Aula - 100
+        Curso - 101
+        102
+ ''',
+                                  epilog="final linha")
+
+
+comados.add_argument('total_pasta', type=int,
+                     help="Valor Total de quantas pasta será criada.")
+comados.add_argument('-n', type=str, nargs='+', default=(),
+                     help="Nome ou Nomes para as pastas")
+comados.add_argument('-p', action="store_true", default=False,
+                     help="Coloca numero depois do Nome da pasta.")
+comados.add_argument('-i', type=int, default=1,
+                     help="Numero que iniciar o valor das pastas")
+comados.add_argument('-s', action="store_true", default=False,
+                     help="Criar as pastas sem fazer pergunta.")
+var = comados.parse_args()
+
+
+if __name__ == '__main__':
+    CreateDir(var.total_pasta, var.n, prefixo=var.p,
+              inicio=var.i, sim=var.s)
